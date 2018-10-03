9b7da6d131665c15529d06e1ad18d060232f5436
Aly Alidina <aly_alidina@hotmail.com>
2018-10-03 11:58:49 -0400

    generated migrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+#creating a new user using perms from stong method user_perms
+  def create
+    @user = User.create(user_perms)
+  end
+
+
+  private
+#prevents hacker from entering different perms then then one in the app
+  def user_perms
+    perms.require(:user).permit(:username, :email, :password)
+  end
 end
